70e5d4fa3a1bf8740a0eefa192c5ebd893385083
Shangchieh Chen <shangchiehchen@ieiworld.com>
2025-06-16 11:23:01 +0800

    500 Data structure algorithm

---
+++ binary/1_multi_16bit_int.c
@@ -0,0 +1,32 @@
+/*
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>			// File Control Definitions.
+#include <termios.h>		// POSIX Terminal Control Definitions.
+#include <unistd.h>			// UNIX Standard Definitions.
+#include <errno.h>			// ERROR Number Definitions.
+#include <pthread.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <time.h>
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
